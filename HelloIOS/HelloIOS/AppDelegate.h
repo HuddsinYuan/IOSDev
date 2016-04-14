a3b6fffe8b82c3811dc9e13459979ab5e7bd77dd
Jonassen Yuan <huddsinyuan2014@gmail.com>
2016-04-14 16:16:04 +0800

    Hello IOS

---
+++ HelloIOS/HelloIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloIOS
+//
+//  Created by 袁晨 on 16/4/14.
+//  Copyright © 2016年 HuddsinYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloIOS/HelloIOSTests/HelloIOSTests.m
@@ -0,0 +1,39 @@
+//
+//  HelloIOSTests.m
+//  HelloIOSTests
+//
+//  Created by 袁晨 on 16/4/14.
+//  Copyright © 2016年 HuddsinYuan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloIOSTests : XCTestCase
+
+@end
+
+@implementation HelloIOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
