77ddb5739e53864eb1748676c4361f9bdfba98c0
Jonassen Yuan <huddsinyuan2014@gmail.com>
2017-03-12 00:00:41 +0800

    Chap02: Add some Image Processing Function.

--- HelloOpenCV/HelloOpenCV/ViewController.mm
+++ HelloOpenCV/HelloOpenCV/ViewController.mm
@@ -20,17 +20,66 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    originImage = [UIImage imageNamed:@"lena.png"];
+    originImage = [UIImage imageNamed:@"computer.jpg"];
     if( originImage != nil ) {
         originImageView.image = originImage;
     }
-    int threshold_value = 0;
     
-    int const max_BINARY_value = 2147483647;
+    
+    
     cv::Mat src_gray=[self cvMatFromUIImage:originImage];
+    
+    originImage = [self cvGetEdge:src_gray];
+    
+    afterImageView.image = originImage;
+    
+}
+
+- (int) calculate:(int) a andb:(int) b
+{
+    int c = a + b;
+    return c ;
+    
+}
+
+- (UIImage*)cvGetEdge:(cv::Mat) src
+{
+    int const max_BINARY_value = 2147483647;
     cv::Mat dst;
-    dst=src_gray;
-    cv::cvtColor(src_gray, dst, cv::COLOR_RGB2GRAY);
+    dst=src;
+    cv::cvtColor(src, dst, cv::COLOR_RGB2GRAY);
+    cv::Mat canny_output;
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+    
+    cv::RNG rng(12345);
+    
+    cv::threshold( dst, dst, 0, max_BINARY_value,cv::THRESH_OTSU );
+    
+    cv::Mat contourOutput = dst.clone();
+    cv::findContours( contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE );
+    
+    //Draw the contours
+    cv::Mat contourImage(dst.size(), CV_8UC3, cv::Scalar(0,0,0));
+    cv::Scalar colors[3];
+    colors[0] = cv::Scalar(255, 0, 0);
+    colors[1] = cv::Scalar(0, 255, 0);
+    colors[2] = cv::Scalar(0, 0, 255);
+    for (unsigned long idx = 0; idx < contours.size(); idx++) {
+        cv::drawContours(contourImage, contours, idx, colors[idx % 3]);
+    }
+    UIImage* retImage =[self UIImageFromCVMat:dst];
+    
+    return retImage;
+}
+
+- (UIImage*)cvThresholding:(cv::Mat) src
+{
+    int threshold_value = 0;
+    int const max_BINARY_value = 2147483647;
+    cv::Mat dst;
+    dst=src;
+    cv::cvtColor(src, dst, cv::COLOR_RGB2GRAY);
     cv::Mat canny_output;
     std::vector<std::vector<cv::Point> > contours;
     std::vector<cv::Vec4i> hierarchy;
@@ -39,11 +88,10 @@
     
     cv::threshold( dst, dst, threshold_value, max_BINARY_value,cv::THRESH_OTSU );
     
-    originImage = [self UIImageFromCVMat:dst];
-    afterImageView.image = originImage;
+    UIImage* retImage = [self UIImageFromCVMat:dst];
     
+    return retImage;
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
